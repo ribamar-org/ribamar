963329ded74a55dd5a26677e28b6edadb83fdf8a
GCSBOSS <gcsgacc@gmail.com>
2018-08-10 14:06:04 -0300

    Add initial unit test cases for database module

--- spec/node.js
+++ spec/node.js
@@ -16,3 +16,89 @@ describe('lib/settings.js', () => {
     it('should overwrite properties when duplicates are present',
         () => assert.equal(testConf.port, 2222));
 });
+
+describe('DataBase', () => {
+    const DataBase = require('../lib/database');
+    let db;
+    let url = process.env.DB_URL || 'mongodb://localhost:27017';
+
+    describe('#connect(settings)', function(){
+
+        beforeEach(function(){
+            db = new DataBase();
+        });
+
+        it('should fail due to missing arguments', async function(){
+            await assert.rejects(async function(){
+                await db.connect();
+            });
+        });
+
+        it.skip('should not connect to inexisting database', async function(){
+            await assert.rejects(async function(){
+                await db.connect({ url: 'blabla', dbName: 'blabla' });
+            });
+        });
+
+        it.skip('should connect to database when settings are valid', async function(){
+            await db.connect({ url: url, dbName: 'nodeTest' });
+            db.close();
+        });
+
+    });
+
+    describe.skip('#insert(collection, doc)', function(){
+
+        beforeEach(async function(){
+            await db.connect({ url: url, dbName: 'nodeTest' });
+        });
+
+        afterEach(function(){
+            db.close();
+        });
+
+        it('should not insert due to missing arguments', async function(){
+            await assert.rejects( async function(){
+                await db.insert();
+            });
+        });
+
+        it('should not insert when when collection name is invalid', async function(){
+            await assert.rejects( async function(){
+                await db.insert('8test$', {});
+            });
+        });
+
+        it('should insert collection when everything is okay', async function(){
+            await db.insert('test', { msg: 'test' });
+        });
+
+    });
+
+    describe.skip('#exists(collection, key, value)', function(){
+
+        beforeEach(async function(){
+            await db.connect({ url: url, dbName: 'nodeTest' });
+        });
+
+        afterEach(function(){
+            db.close();
+        });
+
+        it('should not search due to missing arguments', async function(){
+            await assert.rejects( async function(){
+                await db.exists();
+            });
+        });
+
+
+        it('should find documents collection when everything is okay', async function(){
+            await db.insert('test', { msg: 'test1' });
+            await db.insert('test', { msg: 'test2' });
+            await db.insert('test', { msg: 'test1' });
+            assert(await db.exists('test', 'msg', 'test1'));
+        });
+
+    });
+
+});
