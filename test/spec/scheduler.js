3dcfbd9d245c24421e6a57f8b78a9a4b371b2951
Guilherme C. Souza <gcsgacc@gmail.com>
2018-08-31 10:47:27 -0300

    Add code quality checks to CI and remove dummy scheduler task

--- test/spec/scheduler.js
+++ test/spec/scheduler.js
@@ -6,7 +6,7 @@ describe('Scheduler', () => {
     let flag, bg;
     const Router = require('../../lib/router');
     let router = new Router();
-    router.routes.test = { test: () => flag = true };
+    router.routes[''] = { get: () => flag = true };
 
     describe('#constructor()', function(){
         it('should never fail for any reason', function(){
@@ -34,7 +34,7 @@ describe('Scheduler', () => {
             var s = new Date();
             s.setSeconds(s.getSeconds() + 3);
             s = s.getSeconds();
-            bg.start({ test: s + ' * * * * *' }, router);
+            bg.start({ '': s + ' * * * * *' }, router);
             await new Promise(done => setTimeout(done, 5000));
             assert(flag);
         });
@@ -49,7 +49,7 @@ describe('Scheduler', () => {
             var s = new Date();
             s.setSeconds(s.getSeconds() + 3);
             s = s.getSeconds();
-            bg.start({ test: s + ' * * * * *' }, router);
+            bg.start({ '': s + ' * * * * *' }, router);
             await new Promise(done => setTimeout(done, 1000));
             bg.stop();
             await new Promise(done => setTimeout(done, 5000));
