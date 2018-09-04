e3c3570cabf2e579bcc1ff0c4a4f8081c6f07dc6
Guilherme C. Souza <gcsgacc@gmail.com>
2018-09-04 13:10:06 -0300

    Add support for native object as settings module input

--- test/spec/settings.js
+++ test/spec/settings.js
@@ -4,16 +4,26 @@ var assert = require('assert');
 describe('lib/settings.js', () => {
     const getSettings = require('../../lib/settings');
 
-    it('should return an object when no valid string is present',
-        () => assert.equal(typeof getSettings(undefined), 'object'));
+    it('should return the default settings when missing arguments',
+        () => assert.equal(typeof getSettings(), 'object'));
 
-    it('should return an object when invalid path is present',
-        () => assert.equal(typeof getSettings('./no-file-at-all'), 'object'));
+    it('should return the default settings when invalid path is present',
+        () => assert.equal(getSettings('./no-file-at-all').webserver.port, 6776));
 
-    const testConf = getSettings('./test/settings.yml');
-    it('should apply present properties',
-        () => assert.equal(testConf.property, 'value'));
+    it('should read input yaml file and add its properties to resulting object', function(){
+        let c = getSettings('./test/settings.yml');
+        assert.equal(c.property, 'value');
+    });
 
-    it('should overwrite properties when duplicates are present',
-        () => assert.equal(testConf.port, 2222));
+    it('should read input object file and add its properties to resulting object', function(){
+        let d = getSettings({ webserver: {port: 2222} });
+        assert.equal(d.webserver.port, 2222);
+    });
+
+    it('should overwrite duplicate properties in resulting object', function(){
+        let c = getSettings('./test/settings.yml');
+        assert.equal(c.webserver.port, 2222);
+        let d = getSettings({ webserver: {port: 2222} });
+        assert.equal(d.webserver.port, 2222);
+    });
 });
