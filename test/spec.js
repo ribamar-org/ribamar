4ec1c219b26644018b0c524be14b21d890ce34bc
Guilherme C. Souza <gcsgacc@gmail.com>
2018-09-14 16:15:57 -0300

    Treat exceptions for testing engine's requires

--- test/spec.js
+++ test/spec.js
@@ -3,7 +3,9 @@ var unit = process.argv[5];
 var cases = [ 'settings', 'database', 'router', 'scheduler', 'logger', 'webserver' ];
 
 if(unit)
-    require('./spec/' + unit);
+    try{ require('./spec/' + unit); }
+    catch(e){ console.error(e, `\nModule "${unit}" not found!`); }
 else
     for(let c of cases)
-        require('./spec/' + c);
+        try{ require('./spec/' + c); }
+        catch(e){ console.error(e, `\nModule "${c}" not found!`); }
