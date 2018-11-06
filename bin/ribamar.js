1e10fe48f469e49fae9075edc60ecf4d5e92fbe9
Guilherme C. Souza <gcsgacc@gmail.com>
2018-11-06 13:35:02 -0200

    Fix process not exiting on fatal exception and remove unused env var

--- bin/ribamar.js
+++ bin/ribamar.js
@@ -2,27 +2,22 @@
 
 const Ribamar = require('../lib/main');
 
-const confPath = process.env.RIBAMAR_CONF_PATH || process.argv[2];
-
 console.log(`All paths in conf file should be relative to ${process.cwd()}.`);
 
 var server = new Ribamar();
 
-(async function(){
-    try{
-        await server.start(confPath);
-        console.log(`\nRibamar is listening on TCP port ${server.settings.webserver.port}`);
-    }
-    catch(e){
-        console.error(e.message);
-        process.exit();
-    }
-})();
+function handle(){ server.stop(); }
 
-function handle(){
-    server.stop();
-}
-
-process.on('exit', handle);
 process.on('SIGINT', handle);
 process.on('SIGTERM', handle);
+
+(async function(){
+    try{
+        await server.start(process.argv[2]);
+        console.log(`\nRibamar is listening on TCP port ${server.settings.webserver.port}\n`);
+    }
+    catch(e){
+        console.error(e.message + '\n');
+        process.exit(1);
+    }
+})();
